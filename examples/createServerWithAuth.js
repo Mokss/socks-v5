26fd5f0d03fa6a140b41d70506155c3003a0cf5d
Shuvaev Maksim Elmanovich <mshuvaev@ozon.ru>
2023-08-08 18:31:34 +0300

    - update dependency - fix bug in check auth methods

--- examples/createServerWithAuth.js
+++ examples/createServerWithAuth.js
@@ -1,37 +1,35 @@
-const { createServer } = require("../index");
+import { createServer } from '../index.js';
 
 const server = createServer({
   authenticate(login, password) {
     // verify name/password
-    if (login !== "foo" || password !== "bar") {
-      console.log("authentication failed", login);
+    if (login !== 'foo' || password !== 'bar') {
+      console.log('Authentication failed', login);
       return false;
       // authentication failed
     }
-    console.log(`user ${login} connect`);
+    console.log(`User ${login} connect`);
     // return successful authentication
     return true;
-  }
+  },
 });
 
 server.listen(1080);
 
-server.on("connect", info =>
-  console.log(`connected to remote server at ${info.addr}:${info.port}`)
-);
+server.on('connect', (info) => console.log(`Connected to remote server at ${info.addr}:${info.port}`));
 
-server.on("listening", () => {
+server.on('listening', () => {
   console.log(
-    `server listening ${server.address().address}:${server.address().port}`
+    `Server listening ${server.address().address}:${server.address().port}`,
   );
 });
 
-server.on("connection", socket => {
-  console.log("new socks connection", socket.remoteAddress, socket.remotePort);
+server.on('connection', (socket) => {
+  console.log('New socks connection', socket.remoteAddress, socket.remotePort);
 });
 
-server.on("data", data => console.log(data));
+// server.on('data', (data) => console.log(data));
 
-server.on("error", err => {
-  console.error("server ERROR --->", err);
+server.on('error', (error) => {
+  console.error(error);
 });
